9e7a077488519a6ff845c07916a4b7294918c06f
yuransimao <yuransimao7@gmail.com>
2023-08-26 16:57:39 +0100

    adding task save and render logic

--- Js/main.js
+++ Js/main.js
@@ -1,1 +1,48 @@
+const Button = document.querySelector("button[type='submit']")
+const ListTask = JSON.parse(localStorage.getItem("Task")) || []
+
+async function AddTask() {
+    const Task = await document.querySelector("#Task").value
+    ListTask.unshift({ text: Task })
+
+    localStorage.setItem("Task", JSON.stringify(ListTask))
+    RenderCard()
+}
+
+function RenderCard() {
+    const ContainerTask = document.querySelector("#Container-Task")
+    const RanderTask = new Set()
+
+    if (ListTask) {
+        ContainerTask.innerHTML = ""; 
+        
+        ListTask.forEach(task => {
+
+            if (!RanderTask.has(task.text)) {
+
+                const Card = document.createElement("div")
+                Card.classList.add("Card-Task")
+                Card.innerHTML = `
+                <div class="Task">
+                <h2>${task.text} </h2>
+                </div>
+                `
+                return ContainerTask.appendChild(Card)
+            }
+        })
+    }
+
+}
+
+Button.addEventListener("click", (e) => {
+    e.preventDefault()
+    AddTask()
+})
+
+RenderCard()
+
+
+
+
+
 
